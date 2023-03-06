c555d5f4ad332942bea2ebdcae4f94dab073aa48
Luciara-Abreu <luciara.abreu@gmail.com>
2023-03-06 14:15:02 -0300

    add axios

---
+++ FRONT/src/apiAxios/endPoints/userMain.js
@@ -0,0 +1,78 @@
+const listUsersUrl = "http://localhost:8081/ListAllAnivers";
+const listOneUseUrl = "http://localhost:8081/Aniver/";
+const createUserUrl = "http://localhost:8081/AddAniver";
+const updateUserUrl = "http://localhost:8081/UpdateAniver/";
+const deleteUserUrl = "http://localhost:8081/DeleteAniver/";
+
+const newUSer = {
+  name: "Catarina Master",
+  birthDate: "1994-11-23",
+  sexualOrientation: "F",
+  email: "catarina.m@gmail.com",
+  lastEmail: "-",
+  fone: "21 1231111123",
+};
+
+function getCreateUser() {
+  // eslint-disable-next-line no-undef
+  axios
+    .post(createUserUrl, newUSer)
+    .then((response) => {
+      console.log(response.data);
+      alert(JSON.stringify(response.data));
+    })
+    .catch((error) => console.log(error));
+}
+getCreateUser();
+//---------------------------------------------------------
+export const endpointListAllUser = () => {
+  function getListUsers() {
+    // eslint-disable-next-line no-undef
+    axios
+      .get(listUsersUrl)
+      .then((response) => {
+        const data = response.data;
+        // eslint-disable-next-line no-undef
+        renderResultMes.textContent = JSON.stringify(data);
+        //console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }
+  //return getListUsers();
+};
+//---------------------------------------------------------
+function getListOneUser() {
+  // eslint-disable-next-line no-undef
+  axios.get(listOneUseUrl).then().catch();
+}
+//getListOneUser();
+//---------------------------------------------------------
+const editUser = {
+  name: "Catarina Master",
+  birthDate: "1994-11-23",
+  sexualOrientation: "F",
+  email: "catarina.m@gmail.com",
+  lastEmail: "-",
+  fone: "21 1231111123",
+};
+
+function getUpdateUser() {
+  // eslint-disable-next-line no-undef
+  axios
+    .put(`${updateUserUrl}/id`, editUser)
+    .then((response) => {
+      console.log(response.data);
+      alert(JSON.stringify(response.data));
+    })
+    .catch((error) => console.log(error));
+}
+//getUpdateUser();
+//---------------------------------------------------------
+function getDeleteUser() {
+  // eslint-disable-next-line no-undef
+  axios.get(deleteUserUrl).then().catch();
+}
+//getDeleteUser();
+//---------------------------------------------------------
+
+//export default {}
