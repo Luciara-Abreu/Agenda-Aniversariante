4d61bc3a11c15b9e239a4974f9fffa9b50698f78
Luciara-Abreu <luciara.abreu@gmail.com>
2023-03-07 11:06:17 -0300

    add index pages

---
+++ FRONT/src/pages/index.js
@@ -0,0 +1,14 @@
+
+export * from './login/index'
+export * from './dashboard/index'
+/*
+  export * from './addAniver/index'
+  export * from './consultaMes/index'
+  export * from './criarConta/index'
+  export * from './editAniver/index'
+  export * from './top10/index'
+
+  */
+
+
+
